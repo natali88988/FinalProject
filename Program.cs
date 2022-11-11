4b1315f380c88199843ba7a1681b0c8f0c52b08d
MarsPirs <nkovarskaya98@mail.ru>
2022-11-11 17:49:12 +0300

    Programm for final task

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine($"Введите массив строк через пробел: ");
+string[] inputArray = Console.ReadLine().Split(' ').ToArray();
+string[] outputArray = new string[inputArray.Length];
+
+void CreateOutputArray(string[] inputArray, string[] outputArray) {
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++) {
+    if(inputArray[i].Length <= 3) {
+        outputArray[count] = inputArray[i];
+        count++;
+        }
+    }
+}
+
+CreateOutputArray(inputArray, outputArray);
+Console.WriteLine($"Массив строк, длинна которых меньше или равна 3 символам: ");
+for (int i = 0; i < outputArray.Length; i++) Console.Write($"{outputArray[i]} ");
